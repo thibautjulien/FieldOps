5bc440f4bb5e5fb3386992d828307d5ea960a629
MacBook-Thibaut <thbtjulien@gmail.com>
2026-02-09 11:55:28 +0100

    modification function login

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import authService from "../services/auth.service.js";
+import { Login } from "../services/auth.service.js";
 import User from "../models/User.model.js";
 
 const { JWT_SECRET, JWT_EXPIRES } = process.env;
@@ -7,15 +7,23 @@ const { JWT_SECRET, JWT_EXPIRES } = process.env;
 export async function postAuthLoginController(req, res) {
   try {
     const { email, password } = req.body;
-    if (!email || !password) return res.status(400).json({ error: "Missing email or password" });
+    if (!email || !password)
+      return res.status(400).json({ error: "Missing email or password" });
 
-    const user = await authService.login(email, password);
+    const user = await Login(email, password);
     if (!user) return res.status(401).json({ error: "Invalid credentials" });
 
     const payload = { sub: user.id, email: user.email, role: user.role };
     const token = jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES });
 
-    return res.json({ token, expiresIn: JWT_EXPIRES, id: user.id, name: user.name, email: user.email, role: user.role });
+    return res.json({
+      token,
+      expiresIn: JWT_EXPIRES,
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Internal error" });
--- backend/services/auth.service.js
+++ backend/services/auth.service.js
@@ -1,24 +1,20 @@
 import bcrypt from "bcrypt";
 import User from "../models/User.model.js";
 
-const authService = {
-  login: async (email, password) => {
-    const user = await User.findOne({
-      where: { email },
-    });
+export async function Login(email, password) {
+  const user = await User.findOne({
+    where: { email },
+  });
 
-    if (!user) return (console.log("[FieldOps] User not found"), null);
+  if (!user) return (console.log("[FieldOps] User not found"), null);
 
-    const isValid = await bcrypt.compare(password, user.password);
-    if (!isValid) return (console.log("[FieldOps] Invalid password"), null);
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) return (console.log("[FieldOps] Invalid password"), null);
 
-    return {
-      id: user.id,
-      email: user.email,
-      name: user.name,
-      role: user.role,
-    };
-  },
-};
-
-export default authService;
+  return {
+    id: user.id,
+    email: user.email,
+    name: user.name,
+    role: user.role,
+  };
+}
